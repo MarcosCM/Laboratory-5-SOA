89ba5d8456429a3c6df8f16f5998381ff9952a34
MarcosCM <marketes94@gmail.com>
2015-12-12 18:58:18 +0100

    Lab5 done

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+import java.util.HashMap;
 
 @Controller
 public class SearchController {
@@ -22,7 +24,11 @@ public class SearchController {
 
     @RequestMapping(value="/search")
     @ResponseBody
-    public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+    public Object search(@RequestParam("q") String q, @RequestParam(value = "max", required = false) Integer max) {
+    	Map<String, Object> headers = new HashMap<String, Object>();
+    	headers.put("CamelTwitterKeywords", q);
+    	if (max != null) headers.put("CamelTwitterCount", max);
+    	
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
